8e2a0fa52efab5832d58491aad6a73b06eb9978d
Hyeonji-Ahn <ahj2875@gmail.com>
2023-06-30 22:19:37 -0400

    finished intro to Tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,31 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
-      <div className="bg-white p-6 rounded-2xl shadow-xl">
+    <div className="dark:bg-gray-800 bg-slate-400 lg:place-content-center py-20 px-10 grid gap-10 min-h-screen lg:grid-cols-3">
+      <button 
+      
+      className="bg-white">DarkMode</button>
+      <div className="dark:bg-black dark:text-white bg-white flex flex-col justify-between lg:bg-indigo-600 md:bg-teal-400 sm:bg-red-400 p-6 rounded-2xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between">
-          <span className="text-gray-500 mb-y">Gray Chair</span>
-          <span className= "font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Purple Chair</span>
-          <span className= "font-semibold">$21</span>
-        </div>
+        <ul>
+          {[1,2,3,4,5].map(i =>
+          <div key={i} className="flex justify-between my-2 dark:odd:bg-blue-800 dark:even:bg-red-800 odd:bg-blue-200 even:bg-red-100">
+            <span className="dark:text-white text-gray-500 mb-y">Gray Chair</span>
+            <span className= "font-semibold">$19</span>
+          </div>)}
+        </ul>
+        {["a","b","c",""].map((c,i) => <li key={i} className="empty:bg-red-100 dark:empty:bg-emerald-800">{c}</li>
+        
+        )}
         <div className=" flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className= "font-semibold">$40</span>
         </div>
-        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 
+        <button className=" dark:bg-gray-600 dark:hover:bg-gray-300 dark:hover:text-black mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 
         mx-auto hover:bg-teal-500 active:bg-yellow-500 focus:bg-red-500">Check Out</button>
       </div>
-      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
-        <div className="bg-blue-500 p-6 pb-14">
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl group">
+        <div className="lg:pb-50 md:pb-40 bg-blue-500 p-6 pb-14">
           <span className="text-white text-2xl">Profile</span>
         </div>
         <div className="rounded-3xl  p-6 relative -top-5 bg-white">
@@ -33,19 +38,22 @@ const Home: NextPage = () => {
               <span className= "text-sm text-gray-500">Orders</span>
               <span className = "font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-500 rounded-full"/>
+            <div className="h-24 w-24 bg-red-500 rounded-full group-hover:bg-yellow-500 transition-colors"/>
             <div className= "flex flex-col items-center">
             <span className= "text-sm text-gray-500">Spent</span>
             <span className = "font-medium">$4500</span>
             </div>
           </div>
           <div className ="relative flex flex-col items-center -mt-10 -mb-5">
-            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-lg font-medium group-hover:text-xl group-hover:bg-red-500 transition-all
+            
+            
+            ">Tony Molloy</span>
             <span className="text-sm test-gray-500">America</span>
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl">
+      <div className="bg-white p-10 rounded-2xl shadow-xl md:col-span-2 lg:col-span-1">
         <div className="flex mb-5 justify-between items-center">
           <span>⬅</span>
           <div className="space-x-3">
